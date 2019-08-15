e2ae9d4cc066b3b5c2e7f9e15d78d306a08b75b8
Yun Huang Yong <yun@nomitor.com>
2019-08-15 18:37:30 +1000

    multicheckbox dirty demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {FormGroup} from '@angular/forms';
+import {FormlyFieldConfig} from '@ngx-formly/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ngx-formly-multicheckbox-dirty';
+  form = new FormGroup({});
+  model = { email: 'email@gmail.com' };
+  fields: FormlyFieldConfig[] = [
+    {
+      key: 'text',
+      type: 'input',
+      templateOptions: {
+        label: 'Text',
+      },
+    },
+    {
+      key: 'interest',
+      type: 'multicheckbox',
+      templateOptions: {
+        label: 'Interest',
+        options: [
+          {
+            key: 'sports',
+            value: 'Sports'
+          },
+          {
+            key: 'movies',
+            value: 'Movies'
+          },
+          {
+            key: 'others',
+            value: 'Others'
+          }
+        ]
+      }
+    }
+  ];
+
+  submit(model) {
+    console.log(model);
+  }
 }
